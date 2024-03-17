a94387e7692fb0342d531f946b26d330b5330e50
jakradum <145890080+jakradum@users.noreply.github.com>
2024-03-17 21:04:10 +0530

    all 4 checkboxes combinations of passwords done

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   const [state, setState] = useState({ enter: false, home: true });
   const { enter, home } = state;
   const goHome = () => {
-    setState({...state,home:true})
+    setState({ ...state, home: true });
   };
   return (
     <div className="App">
@@ -29,7 +29,7 @@ function App() {
           <ButtonComponent propArray={buttonProps} />
         </div>
       ) : (
-        <PWGenComponent/>
+        <PWGenComponent />
       )}
     </div>
   );
--- src/Password Generator/index.jsx
+++ src/Password Generator/index.jsx
@@ -1,14 +1,16 @@
-import { useState } from "react";
-import { CheckBoxComp } from "../Components/checkbox";
+import { useState } from 'react';
+import { CheckBoxComp } from '../Components/checkbox';
 const PWGenComponent = () => {
   const [state, setState] = useState({
-    capschecked: false,
-    splCharCheck: false,
+    capsChecked: true,
+    splCharCheck: null,
+    numsCheck: null,
+    lowercaseChecked: false,
   });
   function capsCheckFn() {
     setState({
       ...state,
-      capschecked: !capschecked,
+      capsChecked: !capsChecked,
     });
   }
   function splCharCheckFn() {
@@ -17,63 +19,206 @@ const PWGenComponent = () => {
       splCharCheck: !splCharCheck,
     });
   }
-  const { capschecked, splCharCheck } = state;
+  function numsCheckFn() {
+    setState({
+      ...state,
+      numsCheck: !numsCheck,
+    });
+  }
+  function lowercaseCheckFn() {
+    setState({
+      ...state,
+      lowercaseChecked: !lowercaseChecked,
+    });
+  }
+  const { capsChecked, splCharCheck, numsCheck, lowercaseChecked } = state;
   const numberProps = [
     {
-      id: "number",
-      name: "number",
-      value: "number",
-      label: "Number",
-      ischeckedfn: "",
-      handleChange: "",
+      id: 'number',
+      name: 'number',
+      value: 'number',
+      label: 'Number',
+      ischeckedfn: numsCheck,
+      handleChange: numsCheckFn,
     },
   ];
   const splCharProps = [
     {
-      id: "Special characters",
-      name: "Special characters",
-      value: "",
-      label: "Special characters",
+      id: 'Special characters',
+      name: 'Special characters',
+      value: '',
+      label: 'Special characters',
       ischeckedfn: splCharCheck,
       handleChange: splCharCheckFn,
     },
   ];
   const wordProp = [
     {
-      id: "capital letters",
-      name: "",
-      value: "",
-      label: "CAPITAL LETTERS",
-      ischeckedfn: capschecked,
+      id: 'capital letters',
+      name: '',
+      value: '',
+      label: 'CAPITAL LETTERS',
+      ischeckedfn: capsChecked,
       handleChange: capsCheckFn,
     },
   ];
-  function generateRand(max, min) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-  let pwlength = 6;
-  const capString = () => {
-    let password = "";
-    while (password.length < pwlength) {
-      const capitalLetterRand = generateRand(65, 90);
-      const splCharRand = generateRand(33, 47);
-      let res = String.fromCharCode(capitalLetterRand,splCharRand);
-      password += res;
-    }
-    return password;
-  };
-  const res = capString();
+  const lowercaseProp = [
+    {
+      id: 'small letters',
+      name: '',
+      value: '',
+      label: 'lower case letters',
+      ischeckedfn: lowercaseChecked,
+      handleChange: lowercaseCheckFn,
+    },
+  ];
 
+  let pwLength = 12;
+  let password = '';
+
+  // random number & characters
+  const generateRandom = (min, max) => {
+    const randNum = Math.floor(Math.random() * (max - min + 1) + min);
+    const randChar = String.fromCharCode(randNum);
+    password += randChar;
+  };
+
+  // true values
+  const allValues = [capsChecked, splCharCheck, numsCheck, lowercaseChecked];
+  let trueValues = [];
+  allValues.forEach((v, i) => {
+    if (v) {
+      trueValues.push(v);
+    }
+  });
+
+  // multiple true
+  // caps and lowercase and some other
+  if (trueValues.length >= 2 && capsChecked && lowercaseChecked) {
+    while (password.length < Math.floor(pwLength / 2)) {
+      generateRandom(65, 90);
+      generateRandom(97, 122);
+    }
+    let remaining = pwLength - password.length;
+    if (splCharCheck && numsCheck) {
+      while (remaining > 0) {
+        generateRandom(33, 57);
+        remaining--;
+      }
+    } else if (splCharCheck && !numsCheck) {
+      while (remaining > 0) {
+        generateRandom(33, 47);
+        remaining--;
+      }
+    } else if (numsCheck && !splCharCheck) {
+      while (remaining > 0) {
+        generateRandom(48, 57);
+        remaining--;
+      }
+    }
+  }
+
+  // caps and some other
+  else if (trueValues.length >= 2 && capsChecked) {
+    while (password.length < Math.floor(pwLength / 2)) {
+      generateRandom(65, 90);
+    }
+    let remaining = pwLength - password.length;
+    if (splCharCheck && numsCheck) {
+      while (remaining > 0) {
+        generateRandom(33, 57);
+        remaining--;
+      }
+    }
+    if (splCharCheck) {
+      while (remaining > 0) {
+        generateRandom(33, 47);
+        remaining--;
+      }
+    }
+    if (numsCheck) {
+      while (remaining > 0) {
+        generateRandom(48, 57);
+        remaining--;
+      }
+    }
+  } // lowercase and some other
+  else if (trueValues.length >= 2 && lowercaseChecked) {
+    while (password.length < Math.floor(pwLength / 2)) {
+      generateRandom(97, 122);
+    }
+    let remaining = pwLength - password.length;
+    if (splCharCheck && numsCheck) {
+      while (remaining > 0) {
+        generateRandom(33, 57);
+        remaining--;
+      }
+    }
+    if (splCharCheck) {
+      while (remaining > 0) {
+        generateRandom(33, 47);
+        remaining--;
+      }
+    }
+    if (numsCheck) {
+      while (remaining > 0) {
+        generateRandom(48, 57);
+        remaining--;
+      }
+    }
+  }
+   // caps and lowercase but not others
+   else if (trueValues.length === 2 && capsChecked && lowercaseChecked) {
+    while (password.length < Math.floor(pwLength)) {
+      generateRandom(97, 122);
+      generateRandom(65, 90);
+    }
+  }
+
+  // nums or splchar
+  else if (numsCheck && splCharCheck) {
+    while (password.length < pwLength) {
+      generateRandom(33, 57);
+    }
+  }
+
+  // individual
+  else if (trueValues.length === 1) {
+    if (capsChecked) {
+      while (password.length < pwLength) {
+        generateRandom(65, 90);
+      }
+    }
+    if (splCharCheck) {
+      while (password.length < pwLength) {
+        generateRandom(33, 47);
+      }
+    }
+    if (numsCheck) {
+      while (password.length < pwLength) {
+        generateRandom(48, 57);
+      }
+    }
+    if (lowercaseChecked) {
+      while (password.length < pwLength) {
+        generateRandom(97, 122);
+      }
+    }
+  }
+
+  // JSX for the password string component
   return (
     <div>
       <h2>Strong Password Generator</h2>
       <div className="password ">
-        <h3>{res}</h3>
+        <h3>{password}</h3>
       </div>
+      {/* <p>remaining characters: {pwLength - password.length}</p> */}
       <fieldset className="flex">
         <CheckBoxComp propArray={numberProps} />
         <CheckBoxComp propArray={splCharProps} />
         <CheckBoxComp propArray={wordProp} />
+        <CheckBoxComp propArray={lowercaseProp} />
       </fieldset>
     </div>
   );
