b2d23e8853a25cd05fb6bae47e77dbe4daa27930
jakradum <145890080+jakradum@users.noreply.github.com>
2024-04-19 19:45:18 +0530

    able to play as O for now

--- src/tools/tic-tac-toe.jsx
+++ src/tools/tic-tac-toe.jsx
@@ -1,17 +1,19 @@
 import { gameboard } from './tictactoe-functions';
 import React, { useState } from 'react';
+import { playGame } from './tictactoe-functions';
 
 export const TicTacToe = () => {
   const gameboardArray = Object.values(gameboard);
-  const [selectedCell, setSelectedCell] = useState(null);
-
-  const handleCellClick = (index) => {
-
-  };
+  const [selectedCell, setSelectedCell] = useState('');
+  const [render, setRender] = useState(0);
+  const rerenderFn =()=>{
+    setRender(render + 1);
+  }
 
   const createTable = () => {
     const gridSize = Math.sqrt(gameboardArray.length);
     const tableRows = [];
+
 
     for (let row = 0; row < gridSize; row++) {
       const tableColumns = [];
@@ -19,7 +21,7 @@ export const TicTacToe = () => {
         const index = row * gridSize + col; // Calculate the index based on row and column
         const value = gameboardArray[index]; // Get the value from gameboardArray
         tableColumns.push(
-          <td key={index} onClick={() => handleCellClick(index)}>
+          <td key={index} onClick={() => {setSelectedCell(playGame(index)); rerenderFn()}}>
             {value}
           </td>
         );
--- src/tools/tictactoe-functions.js
+++ src/tools/tictactoe-functions.js
@@ -1,11 +1,16 @@
+
 export const gameboard = {
-    1: '',
-    2: '',
-    3: '',
-    4: '',
-    5: '',
-    6: '',
-    7: '',
-    8: '',
-    9: '',
-  };
+  1: '',
+  2: '',
+  3: '',
+  4: '',
+  5: '',
+  6: '',
+  7: '',
+  8: '',
+  9: '',
+};
+export const playGame = (index) => {
+     gameboard[index+1] = 'O';
+     console.log(gameboard);
+};
