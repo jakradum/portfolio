f16c4e219dfb55acfe2f59105213d3ebed457ee7
jakradum <145890080+jakradum@users.noreply.github.com>
2024-07-01 16:56:06 +0530

    home section + styling of menu done

--- src/ErrorElement.jsx
+++ src/ErrorElement.jsx
@@ -2,7 +2,8 @@ import { NavLink } from "react-router-dom";
 export const ErrorElement = () => {
   return (
     <div>
-      404, bitch
+      <h2>404</h2>
+      <p>I'm not sure what you're looking for in life, but you won't find it here.</p>
       <div>
         <NavLink to="/">Go back home</NavLink>
       </div>
--- src/home.jsx
+++ src/home.jsx
@@ -1,6 +1,18 @@
-import { NavLink, Outlet } from 'react-router-dom';
+import { NavLink, Outlet, useLocation } from 'react-router-dom';
+
+const HomePage = () => {
+  return (
+    <section className='flex'>
+      <h1>Jakradum</h1>
+      <p>I’m copywriter with some interest in coding. Welcome to my website where you’ll find a bunch of my coding projects. Hopefully, I’m adding to this as you read this. If you think it’s been a while since my last project, send me a little motivation by hitting this button below (it’s free). I’m thinking of putting up a blog here with a few of my thoughts on everything under the sun. Cheers.</p>
+      <button>C'mon!</button>
+    </section>
+  );
+};
 
 const Home = () => {
+  const location = useLocation();
+
   return (
     <div>
       <menu className="menu flex">
@@ -19,9 +31,10 @@ const Home = () => {
       </menu>
 
       <main>
-        <Outlet />
+        {location.pathname === '/' ? <HomePage /> : <Outlet />}
       </main>
     </div>
   );
 };
+
 export default Home;
