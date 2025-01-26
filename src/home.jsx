bc83adac763eaa58b67ed4a1b7510223b54b953e
jakradum <pranavkarnad@gmail.com>
2025-01-26 10:58:41 +0530

    partially working mental math tool added

--- src/home.jsx
+++ src/home.jsx
@@ -4,52 +4,57 @@ import { ButtonComponent } from './Components/Button';
 import { WebsitePreview } from './Components/preview';
 import VerticalTimeline from './Components/verticalTimeline';
 import { Logos } from './Components/logos';
+import { HashRouter } from 'react-router-dom';
 
 const HomePage = () => {
   const [count, setCount] = useState(0);
   const counterClick = () => {
     setCount((prevCount) => prevCount + 1);
-  }
-  const buttonProps = [{
-    onclickfn: counterClick,
-    buttonText: `Well done!`,
-  }]
+  };
+  const buttonProps = [
+    {
+      onclickfn: counterClick,
+      buttonText: `Well done!`,
+    },
+  ];
   return (
     <section className="flex">
       <h1>Hello.</h1>
       <p>
-        I’m copywriter with some interest in building websites. Welcome to my website where you’ll find a bunch of my coding
-        projects using JavaScript. Hopefully, I’m adding to this as you read this. If you think it’s been a while since my last project,
-        send me a little motivation by hitting this button below (it’s free). I’m thinking of putting up a blog here
-        with a few of my thoughts on everything under the sun. Cheers.
+        I’m copywriter with some interest in building websites. Welcome to my website where you’ll find a bunch of my
+        coding projects using JavaScript. Hopefully, I’m adding to this as you read this. If you think it’s been a while
+        since my last project, send me a little motivation by hitting this button below (it’s free). I’m thinking of
+        putting up a blog here with a few of my thoughts on everything under the sun. Cheers.
       </p>
-      <ButtonComponent propArray={buttonProps}/>
+      <ButtonComponent propArray={buttonProps} />
       <p>
-        {count > 110
-          ? 'bye'
-          : count > 100
-          ? `${count} times! You need help`
-          : count > 50
-          ? `You just hit that button ${count} times!!!!!!`
-          : count > 30
-          ? `Pls im begging u`
-          : count > 20
-          ? `I'm doing my best`
-          : count > 15
-          ? 'I really got it, stop'
-          : count > 2
-          ? `Thanks, you just said that ${count} times`
-          : count === 2
-          ? `You've said that twice now`
-          : count === 1
-          ? 'Thanks!'
-          : <br/>}
+        {count > 110 ? (
+          'bye'
+        ) : count > 100 ? (
+          `${count} times! You need help`
+        ) : count > 50 ? (
+          `You just hit that button ${count} times!!!!!!`
+        ) : count > 30 ? (
+          `Pls im begging u`
+        ) : count > 20 ? (
+          `I'm doing my best`
+        ) : count > 15 ? (
+          'I really got it, stop'
+        ) : count > 2 ? (
+          `Thanks, you just said that ${count} times`
+        ) : count === 2 ? (
+          `You've said that twice now`
+        ) : count === 1 ? (
+          'Thanks!'
+        ) : (
+          <br />
+        )}
       </p>
       {/* <hr/>
       <h2>More about me</h2>
       <p>My experience</p>
       <VerticalTimeline items={timelineItems}/> */}
-      <Logos/>
+      <Logos />
     </section>
   );
 };
@@ -107,6 +112,11 @@ const Home = () => {
                     Prime Number Finder
                   </NavLink>
                 </li>
+                <li>
+                  <NavLink to="/tools/mental-math" onClick={() => setIsDropdownOpen(false)}>
+                    Mental Math
+                  </NavLink>
+                </li>
               </ul>
             )}
           </li>
@@ -114,11 +124,7 @@ const Home = () => {
       </menu>
 
       <main>
-        {location.pathname === '/' ? (
-          <HomePage />
-        ) : location.pathname.startsWith('/tools') ? (
-          <Outlet />
-        ) : null}
+        {location.pathname === '/' ? <HomePage /> : location.pathname.startsWith('/tools') ? <Outlet /> : null}
       </main>
     </div>
   );
